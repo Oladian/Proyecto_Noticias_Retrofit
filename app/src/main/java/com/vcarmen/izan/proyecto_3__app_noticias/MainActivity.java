1046db2036a122e061e876df2b1cbae071b0b45f
Izan <ios2asir@gmail.com>
2018-12-18 14:10:43 +0100

    Inicio del proyecto

---
+++ app/src/main/java/com/vcarmen/izan/proyecto_3__app_noticias/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vcarmen.izan.proyecto_3__app_noticias;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
